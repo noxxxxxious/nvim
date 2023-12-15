83cc00d2169f1e693c37bfc7a79aaaedd344f12b
noxxxxxious <sean.fried@gmail.com>
2023-12-15 15:18:43 -0600

    Add git status to fugitive keymaps

--- lua/nox/plugins/vim-fugitive.lua
+++ lua/nox/plugins/vim-fugitive.lua
@@ -2,9 +2,11 @@ return {
 	"tpope/vim-fugitive",
 	config = function()
 		vim.keymap.set("n", "<leader>ngg", ":Git<CR>", { desc = "[g]it" })
+		vim.keymap.set("n", "<leader>ngs", ":Git<CR>", { desc = "git [s]tatus" })
 		vim.keymap.set("n", "<leader>ngaa", ":Git add --all<CR>", { desc = "git add --[a]ll" })
 		vim.keymap.set("n", "<leader>ngap", ":Git add --patch<CR>", { desc = "git add --[p]atch" })
 		vim.keymap.set("n", "<leader>ngc", ":Git commit<CR>", { desc = "git [c]ommit" })
-		vim.keymap.set("n", "<leader>ngp", ":Git pull<CR>", { desc = "git [p]ull" })
+		vim.keymap.set("n", "<leader>ngpl", ":Git pull<CR>", { desc = "git pu[l]l" })
+		vim.keymap.set("n", "<leader>ngps", ":Git push<CR>", { desc = "git pu[s]h" })
 	end,
 }
