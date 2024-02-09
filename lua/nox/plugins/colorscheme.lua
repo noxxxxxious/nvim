155537ec3d7617611aa12a01a104f1bf4f272620
noxxxxxious <sean.fried@gmail.com>
2024-02-09 16:45:57 -0600

    revert previous changes

--- lua/nox/plugins/colorscheme.lua
+++ lua/nox/plugins/colorscheme.lua
@@ -3,11 +3,6 @@ return {
 	priority = 1000,
 	config = function()
 		vim.cmd([[colorscheme oxocarbon]])
-		-- Set the background to transparent
-		vim.cmd([[highlight Normal guibg=none]])
-		vim.cmd([[highlight NonText guibg=none]])
-		vim.cmd([[highlight SignColumn guibg=none]])
-		vim.cmd([[highlight LineNr guibg=none]])
 	end,
 }
 
