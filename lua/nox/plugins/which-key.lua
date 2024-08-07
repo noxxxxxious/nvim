08e5e4d280b48cc230b46765bdb322b32cdc05da
noxxxxxious <sean.fried@gmail.com>
2024-08-07 16:30:51 -0500

    fix some weird which-key thing

--- lua/nox/plugins/which-key.lua
+++ lua/nox/plugins/which-key.lua
@@ -5,12 +5,4 @@ return {
 		vim.o.timeout = true
 		vim.o.timeoutlen = 300
 	end,
-	config = function()
-		require("which-key").register {
-			["<Leader>n"] = { name = "[n]ox", _ = 'which_key_ignore' },
-			["<Leader>c"] = { name = "[c]hange", _ = 'which_key_ignore' },
-			["<Leader>s"] = { name = "[s]plit / [s]how", _ = 'which_key_ignore' },
-			["<Leader><Tab>"] = { name = "[tab] commands", _ = 'which_key_ignore' },
-		}
-	end
 }
